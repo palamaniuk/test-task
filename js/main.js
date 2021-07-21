7db6470e449c10166046d8274073e9dbd9d78cec
Max Palamaniuk <83164044+palamaniuk@users.noreply.github.com>
2021-07-21 22:55:45 +0300

    add new files

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(function () {
+  'use strict';
+
+  var boxElem = document.getElementById('box1');
+  var pointerElem = document.getElementById('menu');
+
+  function onMouseMove(event) {
+    var mouseX = event.pageX;
+    var mouseY = event.pageY;
+    var crd = boxElem.getBoundingClientRect();
+    var activePointer = crd.left <= mouseX && mouseX <= crd.right && crd.top <= mouseY && mouseY <= crd.bottom;
+
+    requestAnimationFrame(function movePointer() {
+      if (activePointer) {
+        pointerElem.classList.remove('box-pointer-hidden');
+        pointerElem.style.left = Math.floor(mouseX) + 'px';
+      } else {
+        pointerElem.classList.add('box-pointer-hidden');
+      }
+    });
+  }
+
+  function disablePointer() {
+    pointerElem.style.left = "34%";
+    requestAnimationFrame(function hidePointer() {
+      pointerElem.classList.add('box-pointer-hidden');
+    });
+  }
+
+
+
+  boxElem.addEventListener('mousemove', onMouseMove, false);
+  boxElem.addEventListener('mouseleave', disablePointer, false);
+
+  $('.slider__slide-item').slick({
+    infinite: false,
+    dots: true,
+  });
+});
